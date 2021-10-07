3ae3caad78d069b481513bc2d94ae997061f043d
harini1705 <92089649+harini1705@users.noreply.github.com>
2021-10-07 16:22:28 +0530

    Project 1
    1. C++ Program to find size of Int, Float, Double and Char
    2. C++ Program to find the product of two numbers entered by the user

---
+++ prod.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b,prod;
+	cout<<"\nEnter two numbers: ";
+	cin>>a>>b;
+	prod=a*b;
+	cout<<"\nProduct= "<<prod;
+	return(0);
+}
---
+++ project.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"\nSize of Char: "<<sizeof(char);
+	cout<<"\nSize of Int: "<<sizeof(int);
+	cout<<"\nSize of Float: "<<sizeof(float);
+	cout<<"\nSize of Double: "<<sizeof(double);
+}
